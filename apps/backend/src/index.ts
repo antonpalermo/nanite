11e2101a7c2d9934751727136fe95e3481b15a4a
Anton Angelo Palermo <ANPBusinessmail@gmail.com>
2024-04-24 22:39:54 +0800

    config tsup for database builds

--- apps/backend/src/index.ts
+++ apps/backend/src/index.ts
@@ -1,8 +1,11 @@
 import createServer from "@/server";
+import { initializeMongoConnection } from "@packages/global-database";
 
 async function main() {
   const server = await createServer();
   const port = process.env.PORT || 5555;
+
+  await initializeMongoConnection()
 
   server.get("/status", (_, res) => {
     return res.status(200).json({ status: "healthy", online: true });
