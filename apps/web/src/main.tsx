b4ab55f01cd01e7bca43977794704fca82dacc35
Anton Angelo Palermo <ANPBusinessmail@gmail.com>
2024-04-26 23:27:06 +0800

    chore: init tailwindcss

--- apps/web/src/main.tsx
+++ apps/web/src/main.tsx
@@ -1,8 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
+import App from "@/index.tsx";
+
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <h1>sample</h1>
+    <App />
   </React.StrictMode>
 );
