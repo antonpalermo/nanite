94f163f9e0dffd319252fc8a63eb129057e928fe
Anton Angelo Palermo <ANPBusinessmail@gmail.com>
2024-04-27 00:13:58 +0800

    chore: update tailwind config

--- apps/web/src/index.tsx
+++ apps/web/src/index.tsx
@@ -1,5 +1,13 @@
-import "@/index.css"
+import "@fontsource-variable/inter"
+import "@packages/config-tailwind/styles/base.styles.css"
 
 export default function App() {
-  return <h1 className="text-3xl font-bold underline">Application Sample</h1>;
+  return (
+    <div>
+      <h1 className="text-3xl font-normal">Sphinx of black quartz, judge my vow.</h1>
+      <form action="get">
+        <input type="text"/>
+      </form>
+    </div>
+  );
 }
--- packages/config-tailwind/configs/base.tailwindcss.ts
+++ packages/config-tailwind/configs/base.tailwindcss.ts
@@ -1,7 +1,12 @@
 import { Config } from "tailwindcss";
+import { fontFamily } from "tailwindcss/defaultTheme";
 
 const config: Omit<Config, "content"> = {
   // add global tailwind configs here.
+  theme: {
+    extend: {},
+  },
+  plugins: [require("@tailwindcss/forms")],
 };
 
 export default config;
