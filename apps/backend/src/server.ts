18bdc8b994b5b6c39bccc6238e48272492c4a02f
Anton Angelo Palermo <ANPBusinessmail@gmail.com>
2024-04-30 21:42:01 +0800

    init auth.js

--- apps/backend/src/server.ts
+++ apps/backend/src/server.ts
@@ -3,12 +3,15 @@ import helmet from "helmet";
 import morgan from "morgan";
 import express from "express";
 
+import { ExpressAuth } from "@auth/express";
+
 import { walletRoutes, transactionRoutes } from "@/routes";
 
 export default async function createServer() {
   const app = express();
 
   app.disable("x-powered-by");
+  app.set("trust proxy", true);
 
   app
     .use(cors())
@@ -18,6 +21,7 @@ export default async function createServer() {
     .use(express.urlencoded({ extended: true }));
 
   app
+    .use("/auth/*", ExpressAuth({ providers: [] }))
     .use("/wallet", walletRoutes())
     .use("/transactions", transactionRoutes());
 
